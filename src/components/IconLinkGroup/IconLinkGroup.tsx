57a08dcf2b6ee2d0cb596f463ff0df77c8a04255
daryna-oliinyk <70644536+daryna-oliinyk@users.noreply.github.com>
2022-03-11 17:31:59 +0200

    add IconLinkGroup

---
+++ src/components/IconLinkGroup/IconLinkGroup.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react';
+
+import './IconLinkGroup.scss'
+import { IIconLinkGroup} from "../../types/interphases/IIconLink";
+import {MainColors} from "../../types/enums/MainColors";
+
+import {TargetAttr} from "../../types/enums/TargetAttr";
+import IconLink from "../IconLink/IconLink";
+import {Direction} from "../../types/enums/Direction";
+
+const defaultProps = {
+    color: MainColors.green,
+    target: TargetAttr.blank,
+    direction: Direction.row
+};
+
+const IconLinkGroup: FC<IIconLinkGroup> = ({
+                                     iconLinks,
+                                     color,
+                                     target
+                                 }) => {
+
+    return <div className={"icon-link-group"}>
+        {iconLinks.map((el) =>
+            <IconLink iconLink={el} color={color} target={target}/>
+        )}
+    </div>
+}
+
+IconLinkGroup.defaultProps = defaultProps;
+export default IconLinkGroup
