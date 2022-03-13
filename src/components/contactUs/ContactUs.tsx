30cc58f5b05f52c952da398f29dbccad1c30e330
Serhii Riznyk <riznyk.serhii.v@gmail.com>
2022-03-13 15:12:56 +0200

    ref(form) formatting post data

--- src/components/contactUs/ContactUs.tsx
+++ src/components/contactUs/ContactUs.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./contactUs.scss";
 
 import { Form, Field } from 'react-final-form'
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
 
 import Button from "../Button/Button";
 import Input from "../Fields/Input/Input";
@@ -18,44 +18,56 @@ const instance = axios.create({
     }
 });
 
+
+interface IPostData {
+    data: {
+        "name": string, // Name of the task.
+        "notes": string, // Free-form textual information associated with the task
+        "projects": string[], // Array of project ids.
+        "approval_status"?: "approved" | "changes_requested" | "pending" | "rejected" //Reflects the approval status of this task.
+        "assignee"?: string | null, // Ids of a user.
+        "completed"?: boolean, // True if the task is currently marked complete, false if not.
+        "due_at"?: string | null, // The UTC date and time on which this task is due, or null if the task has no due time.
+        "due_on"?: string, // The localized date on which this task is due, or null if the task has no due date.
+        "followers"?: string[], // An array of strings identifying users.
+        "html_notes"?: string, // The notes of the text with formatting as HTML
+        "start_on"?: string | null, // The day on which work begins for the task , or null. Date format YYYY-MM-DD.
+        "workspace"?: string // Id of a workspace.
+    }
+}
+
 interface IValues {
     firstName: string;
     lastName: string;
     businessemail: string;
     message: string;
 }
+
+
 const ContactUs:React.FC = (): JSX.Element => {
 
     const onSubmit = (values:IValues) => {
         const {firstName,lastName,businessemail,message} = values
-
-                return instance.post('/api/1.0/tasks', {
-                    "data": {
-                        "approval_status": "pending",
-                        "assignee": "1201472437093173",
-                        "completed": false,
-                        //"due_at": '',
-                        "due_on": new Date().toISOString().slice(0, 10),
-                        "followers": [
-                            "1201472437093173"
-                        ],
-                        //"html_notes": "<body>Mittens <em>really</em> likes the stuff from Humboldt.</body>",
-                        "name": `${firstName} ${lastName} Email: ${businessemail}`,
-                        "notes": `${message} `,
-                        "projects": [
-                            "1201830403108446"
-                        ],
-                        "resource_subtype": "default_task",
-                        "start_on": null,
-
-                        "workspace": "1129246741893412"
-                    }
-                }).then(res=>{
-                    const response = res.data;
-                })
+        const data:IPostData = {
+            data: {
+                name: `${businessemail}`,
+                notes: `${firstName} ${lastName}: ${message}`,
+                projects: ["1201830403108446"],
+            }
         }
 
-
+        return instance.post<IValues, AxiosResponse, IPostData>('/api/1.0/tasks', data)
+            .then(res=>{
+            const response = res.data;
+        }).catch((error)=>{
+            if (axios.isAxiosError(error)) {
+                console.log('error message: ', error.message);
+                //@ts-ignore
+                console.log('error: ', error.response.data.errors);
+                return error.message;
+            }
+        })
+    }
 
     return (
         <section className="section contact-us container">
