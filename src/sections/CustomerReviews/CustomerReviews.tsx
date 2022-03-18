4eb6b27b33a45d70e355347d1d01a51883d7d409
miranser <miranserandroid@gmail.com>
2022-03-18 13:44:08 +0200

    Customer Reviews adjustments

--- src/sections/CustomerReviews/CustomerReviews.tsx
+++ src/sections/CustomerReviews/CustomerReviews.tsx
@@ -3,22 +3,26 @@ import './CustomerReviews.scss'
 import { slides } from "./mock";
 import Slider from "../../components/Slider/Slider";
 import { SliderType } from "../../types/enums/Slider/SliderTypes";
-const CustomerReviews = () =>{
-    return (
-        <section className="container--fluid">
-            <h2 id="CustomerReviews">Customer Reviews</h2>
-            
-            <Slider 
-              slides={slides} 
-              variant={SliderType.round}
-              useControls
-              />
-            
-            
-        </section>
+const CustomerReviews = () => {
+  return (
+    <>
+    <div className="container container__customer-reviews">
+    <h2 id="CustomerReviews">Customer Reviews</h2>
+    </div>
+      <section className="container--fluid">
         
-        
-    )
+
+        <Slider
+          slides={slides}
+          variant={SliderType.round}
+          useControls
+        />
+
+
+      </section>
+
+    </>
+  )
 }
 
 export default CustomerReviews 
