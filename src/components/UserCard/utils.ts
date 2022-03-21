d26706cda8351b6541974d1291b0b8e92860b31e
Serhii Riznyk <riznyk.serhii.v@gmail.com>
2022-03-21 10:32:05 +0200

    fix(button): removed Violet Color, hover effects

--- src/components/UserCard/utils.ts
+++ src/components/UserCard/utils.ts
@@ -2,7 +2,7 @@ import {ButtonColors} from "../../types/enums/Button/ButtonColors";
 
 export const getRandomColor = () : ButtonColors => {
     const colors = Object.values(ButtonColors);
-    const randomNumber = Math.floor(Math.random() * 3);
+    const randomNumber = Math.floor(Math.random() * 2);
 
     return colors[randomNumber];
 }
