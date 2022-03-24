ca46da3f997d0a1baeff4fa99fe09934889934d3
miranser <miranserandroid@gmail.com>
2022-03-24 10:54:42 +0200

    responsive slider round mD

--- src/sections/Discover/Discover.tsx
+++ src/sections/Discover/Discover.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 
 import "./Discover.scss"
 import Button from "../../components/Button/Button";
-import {ButtonColors} from "../../types/enums/Button/ButtonColors";
+import { ButtonColors } from "../../types/enums/Button/ButtonColors";
 import { slides } from '../CustomerReviews/mock';
 import { SliderType } from '../../types/enums/Slider/SliderTypes';
 import Slider from '../../components/Slider/Slider';
@@ -10,32 +10,33 @@ import Slider from '../../components/Slider/Slider';
 
 const Discover: FC = () => {
 
-    return <div className={"discover full-width-wrapper"} id={"discover"}>
-        <section className={"discover-section container"}>
-            <div className={"discover__carousel-container"}>
-                <Slider 
-                  slides={slides} 
-                  useControls
-                  infinite 
-                  variant={SliderType.simple}
-                  defaultIndex={0}/>
-            </div>
-            <div className={"discover-content"}>
-                <h2>Discover <br/> who we are</h2>
-                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                <Button
-                    text={"More"}
-                    icon={"angle-right"}
-                    color={ButtonColors.orange}
-                    className={"discover-button"}
-                />
-            </div>
-        </section>
-        <div className={"blue-oval decoration"}/>
-        <div className={"green-oval decoration"}/>
-        <div className={"green-oval second decoration"}/>
-    </div>;
-
+  return (
+    <div className={"discover full-width-wrapper"} id={"discover"}>
+      <section className={"discover-section container"}>
+        <div className={"discover__carousel-container"}>
+          <Slider
+            slides={slides}
+            useControls
+            infinite
+            variant={SliderType.simple}
+            defaultIndex={0} />
+        </div>
+        <div className={"discover-content"}>
+          <h2>Discover <br /> who we are</h2>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          <Button
+            text={"More"}
+            icon={"angle-right"}
+            color={ButtonColors.orange}
+            className={"discover-button"}
+          />
+        </div>
+      </section>
+      <div className={"blue-oval decoration"} />
+      <div className={"green-oval decoration"} />
+      <div className={"green-oval second decoration"} />
+    </div>
+  )
 }
 
 export default Discover
