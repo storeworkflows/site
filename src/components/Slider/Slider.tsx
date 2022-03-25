4918f7e83668a34c0a117ac304b6dc873b9027ac
miranser <miranserandroid@gmail.com>
2022-03-25 14:28:21 +0200

    add touch swipes handling

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,5 @@
-import classnames from "classnames";
-import React, {FC, useState, useRef, useEffect} from "react";
+
+import React, { FC, useState, useRef, useEffect, TouchEvent } from "react";
 import { SliderType } from "../../types/enums/Slider/SliderTypes";
 import { ISlider } from "../../types/interfaces/ISlider";
 import SliderDots from "./SliderDots/SliderDots";
@@ -9,8 +9,8 @@ import './Slider.scss'
 function getInactiveSlideWidth(slidesRefs: Array<HTMLDivElement | null>): number {
   return Math.min(...slidesRefs.map(slide => { return slide?.offsetWidth || 0 }))
 }
-function getTransitionDistance(slideWidth: number, slideIndex: number, variant: SliderType): number{
-  switch(variant){
+function getTransitionDistance(slideWidth: number, slideIndex: number, variant: SliderType): number {
+  switch (variant) {
     case SliderType.round: return (slideWidth / 2) + slideWidth * -slideIndex;
     case SliderType.square: return (slideWidth / 1.35) + slideWidth * -slideIndex;
     case SliderType.simple: return slideWidth * -slideIndex;
@@ -19,19 +19,22 @@ function getTransitionDistance(slideWidth: number, slideIndex: number, variant: 
 }
 const Slider: FC<ISlider> = (
   {
-    slides, 
-    variant, 
-    useDots, 
+    slides,
+    variant,
+    useDots,
     useControls,
     infinite,
     defaultIndex
   }) => {
   const [currentSlide, setCurrentSlide] = useState(defaultIndex)
-  const [transitionStyle, setTransitionStyle] = useState({transform: 'translateX(0)'})
+  const [transitionDistance, setTransirionDistance] = useState(0)
+  const [transitionStyle, setTransitionStyle] = useState({ transform: 'translateX(0)' })
+  const [nextTransition, setNextTransition] = useState(0)
+  const [touchStartPos, setTouchStartPos] = useState(0)
   const length: number = slides.length;
   const slideRefs = useRef<(HTMLDivElement | null)[]>([])
   useEffect(() => {
-    
+
     function handleResize() {
       setActiveSlide(defaultIndex)
     }
@@ -52,42 +55,63 @@ const Slider: FC<ISlider> = (
   }
   const translateCarousel = (slideIndex: number) => {
     const slideWidth = getInactiveSlideWidth(slideRefs.current)
-    const transitionDistance = getTransitionDistance(slideWidth, slideIndex, variant)
-    setTransitionStyle({transform: `translateX(${transitionDistance}px)`})
+    const nextTransitionDistance = getTransitionDistance(slideWidth, slideIndex, variant)
+    setTransitionStyle({ transform: `translateX(${nextTransitionDistance}px)` })
+    setTransirionDistance(nextTransitionDistance)
+  }
+
+  const touchStart = (e: TouchEvent) => {
+    setTouchStartPos(e.touches[0].clientX)
+  }
+  const touchEnd = () => {
+    setTransirionDistance(nextTransition)
+    if (transitionDistance - nextTransition > 100 && currentSlide !== length - 1) nextSlide()
+    else if (transitionDistance - nextTransition < -100 && currentSlide !== -0) prevSlide()
+    else setActiveSlide(currentSlide)
+  }
+  const touchMove = (e: TouchEvent) => {
+    const swipeDistance = e.touches[0].clientX
+    setNextTransition(transitionDistance + swipeDistance - touchStartPos)
+    setTransitionStyle({ transform: `translateX(${nextTransition}px)` })
   }
   return (
     <div className={`slider slider--${variant}`}>
-      <div 
-        className={`slider__carousel slider__carousel--${variant}`} 
-        style={transitionStyle}>
+      <div
+        className={`slider__carousel slider__carousel--${variant}`}
+        style={transitionStyle}
+        onTouchStart={touchStart}
+        onTouchEnd={touchEnd}
+        onTouchMove={touchMove}
+      >
         {
-          slides.map((slide, index)=>(   
-            <Slide 
-              ref={el=>slideRefs.current[index]=el}
-              slide={slide} 
-              variant={variant} 
-              active={currentSlide === index} 
+          slides.map((slide, index) => (
+            <Slide
+              ref={el => slideRefs.current[index] = el}
+              key={index}
+              slide={slide}
+              variant={variant}
+              active={currentSlide === index}
             />
           ))
         }
       </div>
-      {useDots? 
-        <SliderDots 
-        length={length} 
-        active={currentSlide} 
-        clickable 
-        onClick={setActiveSlide}
+      {useDots ?
+        <SliderDots
+          length={length}
+          active={currentSlide}
+          clickable
+          onClick={setActiveSlide}
         />
         : null}
-      {useControls? 
-        <SliderControls 
-          prevDisabled={currentSlide === 0 && !infinite }
+      {useControls ?
+        <SliderControls
+          prevDisabled={currentSlide === 0 && !infinite}
           nextDisabled={currentSlide === length - 1 && !infinite}
           variant={variant}
           onClickPrev={prevSlide}
           onClickNext={nextSlide}
-           />
-          :null}
+        />
+        : null}
     </div>
   )
 
