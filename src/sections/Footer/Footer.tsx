0f4e7d691ad2555fe054fbeb232c67f1bd0e7a36
oleksandr-khudan <oleksandr.khudan@storeworkflows.com>
2022-03-30 11:07:53 +0300

    feat: tablet size
    Tablet size

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import "./Footer.scss"
-import Navigation from "../../components/Navigation/Navigation";
-import IconLinkGroup from "../../components/IconLinkGroup/IconLinkGroup";
+import "./Footer.scss";
 import {navLinks, socialLinks} from "../../constants";
 import {MainColors} from "../../types/enums/MainColors";
+import Navigation from "../../components/Navigation/Navigation";
+import IconLinkGroup from "../../components/IconLinkGroup/IconLinkGroup";
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <Navigation navLinks={navLinks} position="bottom"/>
-      <div className="footer__container container">
-        <div>E-mail</div>
-        <div>Phone number</div>
-        <div>
-          <IconLinkGroup iconLinks={socialLinks} color={MainColors.blue}/>
-        </div>
-        <div>Office Address</div>
-      </div>
-    </footer>
-  )
+	const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+	return (
+		<div className="footer">
+			<div className='footer__wrapper'>
+				<footer className="container">
+					<Navigation className="footer__navigation" mobile={false} navLinks={navLinks} position="bottom"/>
+					<div className="footer__container">
+						<div className="footer__email footer__padding">E-mail</div>
+						<div className="footer__phone footer__padding">Phone number</div>
+						<div className="footer__social footer__padding">
+							<IconLinkGroup iconLinks={socialLinks} color={MainColors.green}/>
+						</div>
+						<div className="footer__address footer__padding">Office Address</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+	)
 }
 
 export default Footer
